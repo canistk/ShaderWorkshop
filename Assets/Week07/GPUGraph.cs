f0f4983270894d06028a794c54ec280b5154e13a
Canis <canistk@gmail.com>
2021-09-11 03:53:46 +0800

    add buffer particle structure

--- Assets/Week07/GPUGraph.cs
+++ Assets/Week07/GPUGraph.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +8,7 @@ public class GPUGraph : MonoBehaviour
     [SerializeField] ComputeShader m_Shader;
     [SerializeField] Material m_Mat;
     [SerializeField] int m_ParticleAmount = 1000;
-    ComputeBuffer m_Buffer;
+    ComputeBuffer m_Particle;
     RenderTexture m_Tex;
 
     private void OnValidate()
@@ -34,19 +34,20 @@ public class GPUGraph : MonoBehaviour
             m_Tex.Release();
             m_Tex = null;
         }
-        m_Buffer.Release();
-        m_Buffer.Dispose();
-        m_Buffer = null;
+        m_Particle.Release();
+        m_Particle.Dispose();
+        m_Particle = null;
     }
 
     public struct Particle
     {
         public Vector3 position;
-        public Quaternion rotation;
         public Vector3 scale;
+        public Vector3 velocity;
         public Color color;
         public float lifetime;
     };
+    private Particle[] m_Buffer;
 
     string GetReadableSize(double len)
     {
@@ -66,20 +67,29 @@ public class GPUGraph : MonoBehaviour
             return;
 
 
-        if (m_Buffer == null)
+        if (m_Particle == null)
         {
             // float = 1 x 4 byte = 4 stride,
             // Vector3 = 3 x 4 bytes = 12stride
             int sizeOfFloat = sizeof(float);
             int sizeOfVector3 = 3 * sizeOfFloat;
             int sizeOfColor = 4 * sizeOfFloat;
-            int sizeOfQuaternion = 4 * sizeOfFloat;
-            int stride = sizeOfVector3 * 2 + sizeOfQuaternion + sizeOfColor + sizeOfFloat;
+            int stride = sizeOfVector3 + sizeOfColor + sizeOfFloat;
 
             string memSize = GetReadableSize(stride * m_ParticleAmount);
 
             Debug.Log($"Alloc GPU memory : stride = {stride}, total : {memSize}");
-            m_Buffer = new ComputeBuffer(m_ParticleAmount, stride);
+            m_Particle = new ComputeBuffer(m_ParticleAmount, stride);
+
+            m_Buffer = new Particle[m_ParticleAmount];
+
+            for (int i = 0; i < m_Buffer.Length; i++)
+            {
+                m_Buffer[i] = new Particle();
+                m_Buffer[i].position = Random.insideUnitCircle * 10f;
+                m_Buffer[i].velocity = Vector2.zero;
+            }
+            m_Particle.SetData(m_Buffer);
         }
 
         if (m_Tex == null)
@@ -91,17 +101,18 @@ public class GPUGraph : MonoBehaviour
         m_Mat.SetTexture("_BaseMap", m_Tex);
         //m_Mat.SetVector("_Color", m_Color);
 
-
-        int kernelHandler = m_Shader.FindKernel("FunctionKernel");
-        if (kernelHandler < 0)
+        int kernelIndex = m_Shader.FindKernel("FunctionKernel");
+        if (kernelIndex < 0)
         {
             Debug.LogError($"Fail to init kernel");
             return;
         }
-        Debug.Log($"Kernel {kernelHandler}");
-        m_Shader.SetTexture(kernelHandler, "Result", m_Tex);
+        Debug.Log($"Kernel {kernelIndex}");
+        m_Shader.SetBuffer(kernelIndex, "Particles", m_Particle);
+        m_Mat.SetBuffer("Particles", m_Particle);
+        m_Shader.SetTexture(kernelIndex, "Result", m_Tex);
         // m_Shader.GetKernelThreadGroupSizes(kernelHandler, out var xs, out var ys, out var zs);
         // Debug.Log($"X {xs}, Y {ys}, Z {zs}");
-        m_Shader.Dispatch(kernelHandler, 256 / 8, 256 / 8, 1);
+        m_Shader.Dispatch(kernelIndex, 256 / 8, 256 / 8, 1);
     }
 }
